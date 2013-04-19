32a937d373c74ad750eb7e1cb540f1dd0f78c04c
Kasun Perera <kkasunperera@gmail.com>
2013-04-19 10:02:08 +0530

    Adding license info to org.karsha.controler package classes

--- src/main/java/org/karsha/controler/SaveDataServelet.java
+++ src/main/java/org/karsha/controler/SaveDataServelet.java
@@ -26,7 +26,20 @@ import org.karsha.data.*;
 import org.karsha.entities.*;
 
 /**
- * Copyright (C) 2012, Lanka Software Foundation.
+ *   Copyright (C) 2013, Lanka Software Foundation and and University of Maryland.
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as
+ *   published by the Free Software Foundation, either version 3 of the
+ *   License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>
  *
  * Date Author Changes April 14, 2012 Kasun Perera Created Jun 18, 2012 Kasun
  * Perera Modified/Removed Unwanted data Structure
