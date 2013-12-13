853ea62eccb259779702d8c9053a122cc2143689
Najini Harischandra <najiniharischandra@ubuntu.ubuntu-domain>
2013-12-13 11:33:36 +0530

    Get similar documents for a single document

--- src/main/java/org/karsha/data/ConnectionPool.java
+++ src/main/java/org/karsha/data/ConnectionPool.java
@@ -58,8 +58,8 @@ public class ConnectionPool {
     //      String password = "admin@lsf";     
            
       String url = "jdbc:mysql://localhost:3306/karshaannotate";      
-            String username = "karshaa";
-          String password = "karshaa";
+            String username = "karsha";
+          String password = "karsha";
             
             /*Credentials for UMIACS server */ 
 //         String url = "jdbc:mysql://localhost:3306/karshaannotate";      
